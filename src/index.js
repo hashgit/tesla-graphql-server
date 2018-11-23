9ba3775d9568d98b2e5d543491522b833e912e16
imad <immaad@gmail.com>
2018-11-23 18:17:12 +1100

    serving static file as well

--- src/index.js
+++ src/index.js
@@ -38,7 +38,8 @@ app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
-}));
+}))
+.use('/content', express.static('./dist/public/'));
 
 app.listen(4000);
 console.log('Running a GraphQL API server at localhost:4000/graphql');
